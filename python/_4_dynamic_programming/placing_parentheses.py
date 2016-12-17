eebbb13df627e84a9ee59e74314cbfd74284ed85
sergei-doroshenko <6823298@gmail.com>
2016-12-17 11:07:53 +0300

    Solve Placing Parentheses

--- python/_4_dynamic_programming/placing_parentheses.py
+++ python/_4_dynamic_programming/placing_parentheses.py
@@ -1,6 +1,6 @@
 # Uses python3
+# Results: Good job! (Max time used: 0.02/5.00, max memory used: 7725056/536870912.)
 def evalt(a, b, op):
-    # print('{} {} {}'.format(a, op, b))
     if op == '+':
         return a + b
     elif op == '-':
@@ -28,6 +28,9 @@ def print_matrix(m):
 
 
 def get_maximum_value(dataset):
+    if len(dataset) == 1:
+        return int(dataset)
+
     m, M = init_matrixes(dataset)
     # print_matrix(m)
     # print('')
@@ -35,36 +38,39 @@ def get_maximum_value(dataset):
     # print('-------------------------------------------------------')
     n = len(dataset) - (len(dataset) - 1) // 2
 
-    def max_min(y, x, d):
-        t_0, t_1 = 0, 0
-        for k in range(1, d + 1):
-            r = []
-            am = m[y][x - k]
-            bm = m[y + k][x]
-            aM = M[y][x - k]
-            bM = M[y + k][x]
-            op = dataset[x * 2 - 1]
-            t_0 = evalt(am, bm, op)
-            print('{} {} {} = {}'.format(am, op, bm, t_0))
-            t_1 = evalt(aM, bM, op)
-        print('-------------------------------------------------------')
-        return t_0, t_1
+    def max_min(i, j):
+        r = []
+        for k in range(i, j):
+            op = dataset[k * 2 + 1]
+            a = m[i][k]
+            b = m[k + 1][j]
+            t0 = evalt(a, b, op)
+            r.append(t0)
+            # print('({}, {}): ({}, {}) ({}, {}) {} {} {} = {}'.format(i, j, i, k, k + 1, j, a, op, b, t0))
+            c = M[i][k]
+            d = M[k + 1][j]
+            t1 = evalt(c, d, op)
+            r.append(t1)
+            # print('({}, {}): ({}, {}) ({}, {}) {} {} {} = {}'.format(i, j, i, k, k + 1, j, c, op, d, t1))
+        return min(r), max(r)
 
     for s in range(0, n):
         for i in range(0, n - s):
             j = i + s
             if i != j:
-                m[i][j], M[i][j] = max_min(i, j, s)
-                # print('(i:{}, j:{}) = {}'.format(i, j, m[i][j]))
-                # print_matrix(m)
-    print('-------------------------------------------------------')
-    print_matrix(m)
-    print('')
-    print_matrix(M)
+                m[i][j], M[i][j] = max_min(i, j)
+
+    # print('-------------------------------------------------------')
+    # print_matrix(m)
+    # print('')
+    # print_matrix(M)
     return M[0][n - 1]
 
 
 if __name__ == "__main__":
-    # print(get_maximum_value(input()))
+    print(get_maximum_value(input()))
     # print(get_maximum_value('1+5-8'))
-    print(get_maximum_value('5-8+7*4'))
+    # print(get_maximum_value('5-8+7*4'))
+    # print(get_maximum_value('1+5'))
+    # print(get_maximum_value('5-8+7*4-8+9'))
+    # print(get_maximum_value('5'))
